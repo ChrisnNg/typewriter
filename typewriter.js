fe7df4ee15cc23cadf14f38105e42cc566bbed1b
ChrisnNg <cristopherng@hotmail.com>
2019-08-27 17:56:14 +0000

    turned typewriter.js into a function

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,14 @@
-const sentence = "hello there from lighthouse labs";
 let time = 0;
 
-for (const char of sentence) {
+const sentence = function(sentence) {
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, time += 50); // <= 1s delay to make it noticeable. Can dial it down later.
+  }
   setTimeout(() => {
-    process.stdout.write(char);
-  }, time += 50); // <= 1s delay to make it noticeable. Can dial it down later.
-}
-setTimeout(() => {
-  process.stdout.write("\n");
-}, 50 * sentence.length);
+    process.stdout.write("\n");
+  }, 50 * sentence.length);
+};
+
+sentence("hello there from lighthouse labs");
